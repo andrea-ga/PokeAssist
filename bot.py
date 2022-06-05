630ad365dd2a713adcdfc60da37de1a1f4873f73
jollypuppet <galella.andrea@gmail.com>
2022-06-05 14:49:27 +0200

    Adedd fourth Generation to the pokeBase

--- bot.py
+++ bot.py
@@ -488,6 +488,8 @@ def show_pokebase(update: Update, context: CallbackContext):
                     else:
                         count += 1
 
+        #print(mex) #per il debug
+
     update.message.reply_html(mex)
 
 
@@ -508,13 +510,13 @@ def start(update: Update, context: CallbackContext):
                               "<code>/dr folletto spettro</code>\n"
                               "<code>/dr1 coleottero</code>\n"
                               "<u><b>Mostrare Tipi ed Evoluzioni di un Pokémon\n"
-                              "(WIP: al momento sono presenti solo le Generazioni 1, 2 e 3):</b></u>\n"
+                              "(WIP: al momento sono presenti solo le Generazioni 1, 2, 3 e 4):</b></u>\n"
                               "<code>/info nomePokémon</code>\n"
                               "Esempi:\n"
                               "<code>/info bulbasaur</code>\n"
                               "<code>/info charizard</code>\n"
                               "<u><b>Mostrare Nomi e Tipi di tutti i Pokémon\n"
-                              "(WIP: al momento sono presenti solo le Generazioni 1, 2 e 3):</b></u>\n"
+                              "(WIP: al momento sono presenti solo le Generazioni 1, 2, 3 e 4):</b></u>\n"
                               "<code>/all</code>")
 
 
@@ -528,13 +530,13 @@ def help(update: Update, context: CallbackContext):
                               "<code>/dr folletto spettro</code>\n"
                               "<code>/dr1 coleottero</code>\n"
                               "<u><b>Mostrare Tipi ed Evoluzioni di un Pokémon\n"
-                              "(WIP: al momento sono presenti solo le Generazioni 1, 2 e 3):</b></u>\n"
+                              "(WIP: al momento sono presenti solo le Generazioni 1, 2, 3 e 4):</b></u>\n"
                               "<code>/info nomePokémon</code>\n"
                               "Esempi:\n"
                               "<code>/info bulbasaur</code>\n"
                               "<code>/info charizard</code>\n"
                               "<u><b>Mostrare Nomi e Tipi di tutti i Pokémon\n"
-                              "(WIP: al momento sono presenti solo le Generazioni 1, 2 e 3):</b></u>\n"
+                              "(WIP: al momento sono presenti solo le Generazioni 1, 2, 3 e 4):</b></u>\n"
                               "<code>/all</code>")
 
 
